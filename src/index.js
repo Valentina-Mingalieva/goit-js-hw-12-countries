5a436f215065e9c5f4749ef5d86e4eabc92d52d6
Valentina Mingalieva <valentina.mingalieva@gmail.com>
2021-07-04 22:55:28 +0200

    two templates

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
+'use strict';
+
 import './sass/main.scss';
 import fetchCountries from "./js/fetchCountries.js";
 import countryTemplate from './partials/countryCard.hbs';
+import renderListTemplate from './partials/renderListTemplate.hbs';
 
 import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
 import { defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import { alert } from '@pnotify/core';
+import * as Confirm from "@pnotify/confirm";
+import "@pnotify/confirm/dist/PNotifyConfirm.css";
 import "@pnotify/core/dist/PNotify.css";
 import "@pnotify/core/dist/BrightTheme.css";
 defaultModules.set(PNotifyMobile, {});
-import { alert, error } from '@pnotify/core';
 
 const debounce = require('lodash.debounce');
 
 const input = document.querySelector('.input');
 const list = document.querySelector('.list');
-const countryContainer = document.querySelector('.country-container')
+const countryContainer = document.querySelector('.country-container');
 
 function onInputChange() {
   countryContainer.innerHTML = '';
@@ -21,15 +26,15 @@ function onInputChange() {
 
   fetchCountries(input.value)
     .then(renderCountries)
-    .catch(alert({ text: 'Please enter a country!' }))
+    .catch()
     .finally()
 }
 
 function renderCountries(countries) {
   if (countries.length > 1 && countries.length <= 10) {
-    list.appendChild(countries);
+    renderList(countries);
   } else if (countries.length > 10) {
-    alert({ text: 'To many matches found. Please enter a more specific query!' });
+    alert({ text: 'Too many matches found. Please enter a more specific query!' });
   } else {
     renderCountry(countries);
   }
@@ -37,6 +42,10 @@ function renderCountries(countries) {
   
 function renderCountry(country) {
   countryContainer.insertAdjacentHTML('beforeend', countryTemplate(country));
+}
+
+function renderList(countries) {
+  list.insertAdjacentHTML('beforeend', renderListTemplate(countries));
 }
   
 input.addEventListener('input', debounce(onInputChange, 500));
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,4 +1,4 @@
-const BASE_URL = `https://restcountries.eu/rest/v2/`;
+const BASE_URL = `https://restcountries.eu/rest/v2/name/`;
 
 export default function fetchCountries(searchQuery) {
     return fetch(`${BASE_URL}${searchQuery}`)
